3e95abb85886a2d1b8e9afa60a1eb602c1cb3d53
Van Trinh <trinh22v@mtholyoke.edu>
2018-02-15 13:28:38 -0500

    Finish sorting by first word in each line

--- textsort2.c
+++ textsort2.c
@@ -1,44 +1,63 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX_LINE_LEN 128
+#define MAX_FILE_LEN 150
 
 int compare(const void *elem1, const void *elem2);
+void printLines (char *ptArray[]);
+
 char line1[] = "bcdea";
 char line2[] = "Nate is cute";
-int values[] = {1, 2, 6, 7, 9};
-char *p1 = NULL;
-char *p2 = NULL;
- 
+char line3[] = "hi there";
+
+// Define an array of pointers, each element points to a char
+char *ptArray[MAX_LINE_LEN];
+
+// Methods to count words on line, check lines and print out lines
+int getLineLength(char line[]);
 
 int main(int argc, char *argv[]){
-  printf("Before sorting: \n");
-  printf("Line 1 is: %s \n", line1);
-  printf("Line 2 is: %s \n", line2);
+  ptArray[0] = line1;
+  ptArray[1] = line2;
+  ptArray[2] = line3;
+  /* printf("First element in ptArray is: %s \n", ptArray[0] ); */
+  /* printf("Second element in ptArray is: %s \n", ptArray[1]); */
+  printLines(ptArray);
   // Second parameters is number of characters NOT INCLUDING '\0' in the end
-  qsort(line1, 5, sizeof(char), compare);
 
-  //p1 = &line1[0];
-  //p2 = &line1[1];
-  //int i = compare(p1, p2);
-  //printf("i is: %d\n", i);
-  printf("After sorting: \n");
-  printf("Line 1 is: %s \n", line1);
-  printf("Line 2 is: %s \n", line2);
+  qsort(ptArray, 3, sizeof(char *), compare);
+  /* printf("After sorting: \n"); */
+  /* printf("Line 1 is: %s \n", line1); */
+  /* printf("Line 2 is: %s \n", line2); */
+  printLines(ptArray);
+  
   return 0;
+}
+
+int getLineLength(char line[]) {
+  int counter;
+  for (counter = 0; (counter < MAX_LINE_LEN - 1); counter++) {
+    if (line[counter] == '\0') {
+      break;
+    }
+  }
+  return counter;
 }
 
 //  do the pairwise comparison of values in the array
 int compare(const void *elem1, const void *elem2) {
-   printf("Start \n");
+  printf("Start \n");
   /* Cast to its actual type */
-  char *strptr1 = (char *) elem1;
+  char **strptr1 = (char **) elem1;
    
-  char *strptr2 = (char *) elem2;
+  char **strptr2 = (char **) elem2;
    
   /* Dereference to get the strings */
-  //char *str1 = *strptr1;
+  char *str1 = *strptr1;
 
-  //char *str2 = *strptr2;
+  char *str2 = *strptr2;
 
    
   // allocates memory for the copy
@@ -52,8 +71,8 @@ int compare(const void *elem1, const void *elem2) {
   //int i = 0; // counter
   // loops the rest of the line until reaching key
   // while( i <= key && (chop1 != NULL ))  {
-    //  while ((word = strtok(NULL, " ")) != NULL)
-    //    printf("Next: %s\n", word);
+  //  while ((word = strtok(NULL, " ")) != NULL)
+  //    printf("Next: %s\n", word);
   //}
   /* Then use strcmp to compare the strings */
   
@@ -62,9 +81,14 @@ int compare(const void *elem1, const void *elem2) {
   
   //  To chop lines into words, you can use strtok(). Be careful, though; it is destructive and will change the contents of the lines. Thus, if you use it, make sure to make a copy of the original line for later use.
 
-   // Use the pointers, not the strings themselves for strcmp function
-   int i = strcmp(strptr1, strptr2);
-   printf("Line 1 is: %s \n", line1);
-   printf("Compare function result: %d \n", i);
-   return i;
+  // Use the pointers, not the strings themselves for strcmp function
+  int i = strcmp(str1, str2);
+  printf("Compare function result: %d \n", i);
+  return i;
+}
+
+void printLines (char *ptArray[]) {
+  for (char **a = ptArray; *a; a++) {
+    printf("%s\n", *a);
+  }
 }
